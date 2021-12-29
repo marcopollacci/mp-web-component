3875e042844be6f69a0072204ebd18fc3b9ec29f
Marco Pollacci <pollacci@gmail.com>
2021-12-29 17:58:01 +0100

    Change sed

--- scripts/font-path.sh
+++ scripts/font-path.sh
@@ -6,9 +6,9 @@ echo $branch;
 
 if [ "$branch" = "master" ]
 then
-   sed -i 's/build\/assets\/fa\/webfonts/https:\/\/web-component.marcopollacci.it\/assets\/fa\/webfonts/' src/components/mp-font-awesome/mp-font-awesome.scss
+   sed -i 's/build\/assets\/fa\/webfonts/https:\/\/web-component.marcopollacci.it\/build\/assets\/fa\/webfonts/' src/components/mp-font-awesome/mp-font-awesome.scss
 else
-   sed -i 's/build\/assets\/fa\/webfonts/https:\/\/develop.mp-web-component.pages.dev\/assets\/fa\/webfonts/' src/components/mp-font-awesome/mp-font-awesome.scss
+   sed -i 's/build\/assets\/fa\/webfonts/https:\/\/develop.mp-web-component.pages.dev\/build\/assets\/fa\/webfonts/' src/components/mp-font-awesome/mp-font-awesome.scss
 fi
 
 
